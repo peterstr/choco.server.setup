94404f1dfe9ecec34cddd98381f162b92e4d1aca
Peter Stræde <peter@headdown.dk>
2020-09-14 22:17:19 +0200

    Create chocoscript.bat
    Initiale file commit - created bat script for notepad++, git, azure cli and azure storage exporer

---
+++ chocoscript.bat
@@ -0,0 +1,14 @@
+#echo installing notepad++
+choco install notepadplusplus.install
+
+#echo installing git
+choco install git
+
+#echo installing azure cli
+choco install azure-cli
+
+#echo installing azure storage explorer 
+choco install microsoftazurestorageexplorer
+
+
+
