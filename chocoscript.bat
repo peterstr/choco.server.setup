b6aa32689b664d7631bbf972fb768d6e16068826
Peter Stræde (XPEST-ADMIN) <XPEST-ADMIN@energinet.local>
2020-09-16 14:04:54 +0200

    added stuff

--- chocoscript.bat
+++ chocoscript.bat
@@ -1,5 +1,5 @@
-#echo installing notepad++
-choco install notepadplusplus.install
+#echo installing notepad++ (32bit 
+choco install notepadplusplus --x86 
 
 #echo installing git
 choco install git
@@ -9,6 +9,18 @@ choco install azure-cli
 
 #echo installing azure storage explorer 
 choco install microsoftazurestorageexplorer
+
+#echo installing ReSharper
+choco install resharper
+
+#echo installing putty
+choco install putty.install
+
+#echo installing Postman
+choco install postman
+
+#echo installing terraform
+choco install terraform
 
 
 
